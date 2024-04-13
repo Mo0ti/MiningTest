466cd09849dfe4690a5f6b8640c1c35cec3645eb
Mo0ti <82755644+Mo0ti@users.noreply.github.com>
2024-04-13 03:49:58 +0300

    Update telegram.js

--- Build/telegram.js
+++ Build/telegram.js
@@ -1,2 +1,13 @@
-const tele = window.Telegram.WebApp;
-tele.expand();
+    let tg      = window.Telegram;
+  
+    if(tg != undefined){
+      if (tg.WebApp != undefined && tg.WebApp.initData != undefined){
+       
+      let safe    = tg.WebApp.initData;
+      
+      tg.WebApp.backgroundColor = '#3d3d3d';
+      tg.WebApp.headerColor = '#212121';
+      tg.WebApp.expand(); 
+  
+      }    
+    }
