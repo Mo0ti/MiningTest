86f7fee9879eb390942b95a3594dc66afa2dcfa3
Mo0ti <82755644+Mo0ti@users.noreply.github.com>
2024-04-16 00:22:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 let tg = window.Telegram.WebApp;
 
- if (tg.WebAppUser && tg.WebAppUser.username != null) {
-        tg.showAlert(Добро пожаловать, @${tg.WebAppUser.username}.);
-    } 
- else {
-        tg.showAlert(Добро пожаловать, пользователь без имени.);
-    };
+tg.showAlert(`Добро пожаловать, @${tg.WebAppUser.username}.`);
 
 tg.expand();
