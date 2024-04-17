c52e943036046d8bcbdcf9086c0e818a2d084216
Mo0ti <82755644+Mo0ti@users.noreply.github.com>
2024-04-18 00:15:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ var tg = window.Telegram.WebApp;
 
 tg.expand();
 
-(function($){
-$(function() {
-  var userName = tg.tg.initDataUnsafe.user.username;
-  var mes = "hello "+userName+", how are you doing";
-  tg.showAlert(mes);
-}
-}(jQuery));
-
 //(function($){
-//tg.showAlert(`Добро пожаловать, ${tg.WebAppUser.username}.`);
+//$(function() {
+//  var userName = tg.tg.initDataUnsafe.user.username;
+//  var mes = "hello "+userName+", how are you doing";
+//  tg.showAlert(mes);
+//}
 //}(jQuery));
+
+(function($){
+tg.showAlert(`Добро пожаловать, ${tg.initDataUnsafe.user.username}.`);
+}(jQuery));
